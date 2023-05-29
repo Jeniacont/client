0b0abfefbc124aecdfd1731b535e2bb74a4ad6ec
Jeniacont <jeniabortnic@gmail.com>
2023-05-29 07:30:36 +0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AppRoutes from "./AppRoutes";
 
 const App = () => {
-  return <div> App</div>;
+  return (
+    <div className="container">
+      <AppRoutes />
+    </div>
+  );
 };
 
 export default App;
